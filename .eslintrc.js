17ed85e424d054ab9755a548b674d55c43b8fdee
Unknown <abodsinan96@gmail.com>
2020-11-06 18:38:36 +0800

    Formatting config
    updated formatting config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,29 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    mocha: true
+  },
+  extends: [ 'airbnb-base' ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    indent: [ 'error', 2 ],
+    quotes: [ 'error', 'single' ],
+    semi: [ 'error', 'always' ],
+    'no-console': 1,
+    'comma-dangle': [ 0 ],
+    'arrow-parens': [ 0 ],
+    'object-curly-spacing': [ 'warn', 'always' ],
+    'array-bracket-spacing': [ 'warn', 'always' ],
+    'import/prefer-default-export': [ 0 ],
+    'linebreak-style': 0,
+  }
 };
